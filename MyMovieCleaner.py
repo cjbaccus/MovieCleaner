33bae47940a670f7143d5158b4803a354352c0fb
cjbaccus <cjbaccus@gmail.com>
2016-11-22 06:06:38 -0800

    Added rating to movie folder name and cleaned extra print debugging statements

--- MyMovieCleaner.py
+++ MyMovieCleaner.py
@@ -8,23 +8,23 @@ import json
 import re
 import wget
 
-folder2save = "Q:\movies\\"
+folder2save = "C:\Users\cbaccus\Desktop\CleanerTest\MovieCleaner\Movies\\"
 
 for x in os.listdir("./"):
-	print x
-	y = re.match(r"(.*)\.mkv", str(x))
+	#print x
+	y = re.match(r"(.*)\.mp4", str(x))
 	if y:
 		t = y.group(1)
 		url='http://www.omdbapi.com/?t='+str(t)
 		# moviename='Q:\\'+ t
 		response = urllib.urlopen(url).read()
 		jsonvalues = json.loads(response)
-		print jsonvalues
+		#print jsonvalues
 		if jsonvalues["Response"]=="True":
 				imdbrating = jsonvalues['imdbRating']
-				print imdbrating+" "+x
-				destinationDir = folder2save + jsonvalues['Title']
-				print destinationDir
+				#print imdbrating+" "+x
+				destinationDir = folder2save + imdbrating + "_" + jsonvalues['Title']  
+				#print destinationDir
 				if not os.path.exists(destinationDir): 
 					os.makedirs(destinationDir)
 				imurl = jsonvalues['Poster']
